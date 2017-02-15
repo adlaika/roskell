ffc61200e9dc96cbde0624a3bdca7110abcf7986
m-arnold <michaelaarnold@gmail.com>
2017-02-14 21:27:25 -0800

    add minimal generateLevel

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,22 +1,33 @@
 module Lib
-  ( Piece
-  , Loc
-  , Board 
+  ( Piece(..)
+  , Loc(..)
+  , Level(..)
+  , Action(..)
+  , generateLevel
   ) where
 
-import Data.Aeson
+import Data.Map as M
 
 data Piece
   = Player
   | Wall
+  | Monster
   deriving (Show, Eq)
 
 data Loc = Loc
   { _locX :: Int
-  , _LocY :: Int
-  } deriving (Show, Eq)
+  , _locY :: Int
+  } deriving (Show, Eq, Ord)
 
-data Board = Board
-  { _bCells :: (M.Map Loc Piece)
+data Level = Level
+  { _bCells :: M.Map Loc Piece
   , _bSize :: Int
   } deriving (Show, Eq)
+
+data Action
+  = Fight
+  | Walk
+  deriving (Show, Eq)
+
+generateLevel :: Int -> Level
+generateLevel size = Level { _bCells = M.empty, _bSize = size }
--- test/LibSpec.hs
+++ test/LibSpec.hs
@@ -2,13 +2,20 @@
 
 module LibSpec (spec) where
 
+import Data.Map as M
+
 import Test.Hspec
 
 import Lib
 
 spec :: Spec
 spec = do
-  describe "Board" $ do
-    context "mkBoard" $ do
-      it "creates a 3x3 board" $
-        True `shouldBe` True
+  describe "Level" $ do
+    context "generateLevel" $ do
+      it "generates a size 0 level" $ do
+        let expected = Level
+              { _bCells = M.empty
+              , _bSize = 0
+              }
+        let actual = generateLevel 0
+        expected `shouldBe` actual
