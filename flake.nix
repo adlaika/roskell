ea569da7354d5234fb9011bfe30f3983c05701ba
Alexander Tomokhov <alexoundos@gmail.com>
2024-02-13 07:27:30 +0400

    build with Nix flake

---
+++ flake.nix
@@ -0,0 +1,36 @@
+{
+  description = "rogue-like game written in Haskell";
+  inputs.nixpkgs.url = "nixpkgs";
+  outputs = { self, nixpkgs }:
+    let
+      supportedSystems = [ "x86_64-linux" "x86_64-darwin" ];
+      forAllSystems = f: nixpkgs.lib.genAttrs supportedSystems (system: f system);
+      nixpkgsFor = forAllSystems (system: import nixpkgs {
+        inherit system;
+        overlays = [ self.overlay ];
+      });
+    in
+    {
+      overlay = (final: prev: {
+        roskell = final.haskellPackages.callCabal2nix "roskell" ./. { };
+      });
+      packages = forAllSystems (system: {
+        roskell = nixpkgsFor.${system}.roskell;
+      });
+      defaultPackage = forAllSystems (system: self.packages.${system}.roskell);
+      checks = self.packages;
+      devShell = forAllSystems (system:
+        let haskellPackages = nixpkgsFor.${system}.haskellPackages;
+        in haskellPackages.shellFor {
+          packages = p: [ self.packages.${system}.roskell ];
+          withHoogle = true;
+          buildInputs = with haskellPackages; [
+            haskell-language-server
+            ghcid
+            cabal-install
+          ];
+          # Change the prompt to show that you are in a devShell
+          shellHook = "export PS1='\\e[1;34mdev > \\e[0m'";
+        });
+    };
+}
